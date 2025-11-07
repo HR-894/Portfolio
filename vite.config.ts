9be19ff5031c2012e18b5b62ed3973d7e43ea212
HIMANSHU RAJ <contacthimanshu222@gmail.com>
2025-11-07 09:20:51 +0530

    Add vite-plugin-sitemap to generate sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import sitemap from "vite-plugin-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +9,9 @@ export default defineConfig({
     host: "::",
     port: 8080,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({ hostname: "https://portfolio-hr-894.vercel.app" }) ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
